997a3f7c97550f77ac92ccdbad1044b7d018b508
Andrey <andrey.luzhkovsky@codex-soft.com>
2014-11-25 16:19:21 +0300

    rest middleware

--- server.js
+++ server.js
@@ -14,56 +14,21 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(ORM.middleware);
+//app.use(ORM.middleware);
 
 
-app.get('/api/pages', function(req, res) {
-	req.db.page.find().exec(function(e, rows){
-		res.json(rows)
-	})
-})
-
-app.post('/api/sites', function(req, res, next) {
-	req.db.site.create(req.body).exec(function(e, obj){
-		req.result = obj;
-		next();
-	})
-})
-
-app.get('/api/sites', function(req, res, next) {
-	req.db.site.find().exec(function(e, rows){
-		req.result = rows;
-		next();
-	})
-})
-
-
-app.delete('/api/sites/:id', function(req, res, next) {
-	req.db.site.destroy(req.params.id).exec(function(e, obj){
-		req.result = obj;
-		next();
-	})
-})
-
-app.put('/api/sites/:id', function(req, res, next) {
-	req.db.site.update(req.params.id, req.body).exec(function(e, rows){
-		req.result = rows[0];
-		next();
-	})
-})
-
-app.use(function(req, res, next){
-	console.log('RETURN JSON--');
-
-	if (req.result)
-		res.json(req.result)	
-	else
-		next();
-})
 
 
 ORM.init(app, function(e){
+    // app.use('/api/sites', ORM.REST('site').get('/:id/publish', function(req, res){
+    // 		res.send('ok')
+    // 	}))
+       
+  	app.use('/api/sites', ORM.REST('site'))
+    app.use('/api/pages', ORM.REST('page'));
+
 	app.listen(config.port, function () {
         console.log('http://localhost:' + config.port);
     });
+
 })
